a95e6b68b762bee620bf129ce83e9f9bb1a7898c
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-09-21 20:40:21 -0600

    pushing code for challenge 3, all tests passing

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -7,14 +7,11 @@ Write a function called addTwo that takes in an array and adds two to every valu
 ------------------------------------------------------------------------------------------------ */
 
 const addTwo = (arr) => {
-  let newArray1 = [];
-  for (let i = 0; i <= 0; i++) {
-    arr.map(i => {
-      newArray1.push(i + 2);
-    });
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] + 2);
   }
-
-  return newArray1;
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -26,23 +23,10 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const typeNum = (arr) => {
-  return arr.filter(element => !isNaN(element))
+  return arr.filter(x => {
+    if (!isNaN(x)) { return true }
+  });
 };
-
-  // Below code not working
-  /*
-  let newArray2 = [];
-  let numbersOnly = (element) => {
-    if (typeof(element) === 'number') {
-      return element;
-    }
-  };
-
-  newArray2.push(arr.filter(numbersOnly));
-
-  return newArray2;
-};
-*/
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
@@ -53,7 +37,9 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-  return arr.filter(element => element.includes('and'));
+  return arr.filter((x) => {
+    return (x.includes('and'));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -65,7 +51,7 @@ For example, oddValues([1,2,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const oddValues = (arr) => {
-  return arr.filter(element => (element % 2 == 1));
+  return arr.filter(num => (num % 2));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -77,8 +63,9 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  return arr.filter(element => !forbiddenValues.includes(element));
-
+  return arr.filter((val) => {
+    return !forbiddenValues.includes(val);
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
