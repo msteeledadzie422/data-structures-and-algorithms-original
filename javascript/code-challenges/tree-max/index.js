416dda0871dad05f68483e14bb9137cbeb6ba8a8
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-10-10 17:13:37 -0600

    pushing code for challenge 16

--- javascript/code-challenges/tree-max/index.js
+++ javascript/code-challenges/tree-max/index.js
@@ -93,6 +93,25 @@ class BinarySearchTree extends BinaryTree {
     return false;
   }
 
+  getMax() {
+    let tempValueArray = [];
+    const traverse = node => {
+      tempValueArray.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(this.root);
+    let maxValue = 0;
+    tempValueArray.forEach(value => {
+      if (value > maxValue) {
+        maxValue = value;
+      }
+      return maxValue;
+    });
+    return maxValue;
+  }
+
 }
 
 let tree = new BinaryTree();
